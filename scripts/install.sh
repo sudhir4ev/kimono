52e3df1776dad3fc71836ae8997eae6760a0003e
Sudhir <>
2023-06-05 11:07:53 +0800

    add vite-plugin-medusa setup scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,13 @@
-cd tools/wetkit
+#!/bin/bash
+CWD=$(pwd)
+
+cd $CWD/tools/vite-plugin-medusa
 yarn install
 yarn build
 
-cd ../../
+cd $CWD/tools/wetkit
+yarn install
+yarn build
+
+cd $CWD
 yarn install --force
